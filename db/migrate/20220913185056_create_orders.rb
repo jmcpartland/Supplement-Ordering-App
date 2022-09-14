cad38980eceab86e023e160a5c0adf258670f2c1
Joseph McPartland <joemcpartland@gmail.com>
2022-09-14 12:14:39 -0600

    create migration files

--- db/migrate/20220913185056_create_orders.rb
+++ db/migrate/20220913185056_create_orders.rb
@@ -2,6 +2,8 @@ class CreateOrders < ActiveRecord::Migration[6.1]
   def change
     create_table :orders do |t|
       t.integer :order_number
+      t.integer :quantity
+      t.decimal :price
       t.integer :user_id
       t.integer :supplement_id
 
